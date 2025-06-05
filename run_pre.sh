a6b776a47e809da17624d1eef53280413a682227
nperdi <nikosp@uoa.gr>
2025-06-05 17:48:46 +0300

    ok

--- run_pre.sh
+++ run_pre.sh
@@ -21,6 +21,17 @@ CONFIG_FILE="config_templates/config_nfs_template.tpl"
 OUTPUT_FILE="config_nfs.cfg"
 sed "s#fullpathXXX#${FULLPATH}#g" "$CONFIG_FILE" > "$OUTPUT_FILE"
 
+# fss test case
+CONFIG_FILE="config_templates/test_case_fss_commands.txt"
+OUTPUT_FILE="test_case_fss_commands.txt"
+sed "s#fullpathXXX#${FULLPATH}#g" "$CONFIG_FILE" > "$OUTPUT_FILE"
+
+# nfs test case
+CONFIG_FILE="config_templates/test_case_nfs_commands.txt"
+OUTPUT_FILE="test_case_nfs_commands.txt"
+sed "s#fullpathXXX#${FULLPATH}#g" "$CONFIG_FILE" > "$OUTPUT_FILE"
+
+
 # Προαιρετική αντιγραφή στον πάνω φάκελο
 # cp config_fss.cfg ../
 # cp config_nfs.cfg ../
